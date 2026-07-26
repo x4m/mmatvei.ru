a86585ca7beba6cadcaedef6e3ece676884b2077
Andrey Borodin <amborodin@acm.org>
2026-07-26 08:23:20 +0000

    Threaded HTTPS server with accept timeout (fix for hung single-threaded server)

--- server.py
+++ server.py
@@ -1,20 +1,26 @@
-# taken from http://www.piware.de/2011/01/creating-an-https-server-in-python/
-# generate server.pem with the following command:
-#    openssl req -new -x509 -keyout key.pem -out server.pem -days 365 -nodes
-# run as follows:
-#    python simple-https-server.py
-# then in your browser, visit:
-#    https://localhost:4443
+#!/usr/bin/env python3
+# mmatvei.ru HTTPS static server.
+# Threaded so a single slow/stalled client can't block the whole site,
+# with a socket timeout so half-open TLS handshakes get dropped instead
+# of freezing the accept loop.
 
-
-import http.server
 import ssl
+import http.server
 
-server_address = ('', 443)
-httpd = http.server.HTTPServer(server_address, http.server.SimpleHTTPRequestHandler)
-httpd.socket = ssl.wrap_socket(httpd.socket,
-                               server_side=True,
-                               certfile="/etc/letsencrypt/live/mmatvei.ru/fullchain.pem",
-                               keyfile="/etc/letsencrypt/live/mmatvei.ru/privkey.pem",
-                               ssl_version=ssl.PROTOCOL_TLS)
+CERT = "/etc/letsencrypt/live/mmatvei.ru/fullchain.pem"
+KEY  = "/etc/letsencrypt/live/mmatvei.ru/privkey.pem"
+ADDR = ("", 443)
+
+httpd = http.server.ThreadingHTTPServer(ADDR, http.server.SimpleHTTPRequestHandler)
+httpd.daemon_threads = True
+
+# Drop clients that connect but stall (the failure mode that hung the old
+# single-threaded server). Applies to accept()/handshake on the listen socket.
+httpd.socket.settimeout(20)
+
+ctx = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+ctx.load_cert_chain(certfile=CERT, keyfile=KEY)
+httpd.socket = ctx.wrap_socket(httpd.socket, server_side=True)
+
+print("serving https on :443 (threaded)")
 httpd.serve_forever()
